79b5b8b4b565c1dfba657ee4cd4aa18b88e5b665
abdusalom701 <ulugbekovabdusalom@gmail.com>
2024-08-17 00:50:28 +0500

    js2

---
+++ script.js
@@ -0,0 +1,13 @@
+let  a = prompt("String typedagi ma'lumot kiriting")
+
+let  b = +prompt("Number typedagi ma'lumot kiriting")
+
+let  c =  prompt("Boolean typedagi ma'lumot kiriting") 
+
+alert("hamma malumotlarni to'g'ri kiritganingizga ishonchingiz komilmi*")
+
+console.log(typeof a);
+
+console.log (typeof b);
+
+console.log(typeof c);
